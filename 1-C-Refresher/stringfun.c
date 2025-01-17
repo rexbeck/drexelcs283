412992bdc019899551ea9e7adcaec9b09f6e83b6
rexbeck <reaganrbeck@gmail.com>
2025-01-17 14:20:31 -0500

    Assignment 1 w/ extra credit

--- 1-C-Refresher/stringfun.c
+++ 1-C-Refresher/stringfun.c
@@ -16,57 +16,71 @@ int  setup_buff(char *, char *, int);
 //prototypes for functions to handle required functionality
 int  count_words(char *, int, int);
 //add additional prototypes here
-void reverse_words(char *, char *, int);
+void reverse_words(char *, int);
 void print_words(char *, int);
+void search_and_replace(char *, int, int, char *, char *);
 
 
 int setup_buff(char *buff, char *user_str, int len){
     //TODO: #4:  Implement the setup buff as per the directions
-    int buff_idx = 0, str_idx = 0, inserted_count = 0, str_len = strlen(user_str);
-    bool wasPreviousWhitespace = false;
-    bool isCurrentWhitespace = false;
+    int buff_idx = 0, str_idx = 0, char_count = 0;
+    bool wasPreviousWhitespace = false, isCurrentWhitespace = true;
+    char current = *(user_str + str_idx);
 
-    for(str_idx; str_idx < str_len; str_idx++){
-        if (str_idx > len - 1){
-            printf("The supplied string is too long");
-            return -1;
+    /*Iterates through both the buffer and user string, checking the current character to decide the next
+    course of action.*/
+    while(current != '\0'){
+        if(char_count == len){
+            exit(3);
         }
 
-        char current = user_str[str_idx];
         isCurrentWhitespace = (current == SPACE_CHAR || current == TAB_CHAR);
 
         if(!isCurrentWhitespace){
             *(buff + buff_idx) = current;
-            buff_idx++;
-            inserted_count++;
             wasPreviousWhitespace = false;
+
+            buff_idx++;
+            str_idx++;
+            char_count++;
         }
         else {
-            if(wasPreviousWhitespace){
-                wasPreviousWhitespace = true;
-                continue;
+            //Checks for leading whitespace
+            if (buff_idx == 0){
+                str_idx++;
+            }
+            else if (wasPreviousWhitespace){
+                str_idx++;
             }
             else {
                 *(buff + buff_idx) = SPACE_CHAR;
-                buff_idx++;
-                inserted_count++;
                 wasPreviousWhitespace = true;
+
+                buff_idx++;
+                str_idx++;
+                char_count++;
             }
         }
+
+        current = *(user_str + str_idx);
     }
 
-    for (buff_idx; buff_idx < len; buff_idx++){
-        *(buff + buff_idx) = '.';
+    if(*(buff + buff_idx - 1) == SPACE_CHAR){
+        memset(buff + buff_idx - 1, '.', (len - char_count + 1) * sizeof(char));
     }
-
-    return inserted_count;
+    else {
+        memset(buff + buff_idx, '.', (len - char_count) * sizeof(char));
+    }
+    
+    return char_count;
 }
 
 void print_buff(char *buff, int len){
-    printf("Buffer:  ");
+    printf("Buffer:  [");
     for (int i=0; i<len; i++){
         putchar(*(buff+i));
     }
+    putchar(']');
     putchar('\n');
 }
 
@@ -100,11 +114,18 @@ int count_words(char *buff, int len, int str_len){
     return wc;
 }
 
-void reverse_words(char *buff, char *reversed, int str_len){
-    int buff_idx = str_len - 1, reversed_idx = 0;
-    for(reversed_idx; reversed_idx < str_len; reversed_idx++){
-        reversed[reversed_idx] = buff[buff_idx];
-        buff_idx--;
+void reverse_words(char *buff, int str_len){
+    int start_idx = 0, end_idx = str_len - 1;
+    char temp;
+
+    //Swaps the first and last character in the buffer, iterating inward to the center of the string.
+    while (end_idx > start_idx){
+        memcpy(&temp, buff + end_idx, 1);
+        memcpy(buff + end_idx, buff + start_idx, 1);
+        memcpy(buff + start_idx, &temp, 1);
+
+        start_idx++;
+        end_idx--;
     }
 }
 
@@ -113,45 +134,115 @@ void print_words(char *buff, int str_len){
     int wc = 1;         //counts words
     int wlen = 0;       //length of current word
     bool word_start = false;    //am I at the start of a new word
+    char current;   //current character
 
     printf("Word Print\n----------\n");
 
     for(int i = 0; i < str_len; i++){
+        current = *(buff + i);
         if (word_start) {
-            if (buff[i] == SPACE_CHAR){
+            if (current == SPACE_CHAR){
                 // Just reached the space after the word ended.
                 word_start = false;
-                printf(" (%d)\n", wlen);
+                printf("(%d)\n", wlen);
                 wlen = 0;
             } else {
                 // In a word, continuing to output letters.
-                printf("%c", buff[i]);
+                printf("%c", current);
                 wlen++;
 
                 // Checks if current index is last and returns wordcount.
                 if (i == last_char_idx){
-                    printf(" (%d)\n", wlen);
+                    printf("(%d)\n", wlen);
                 }
             }
         } else {
-            if (buff[i] == SPACE_CHAR){
+            if (current == SPACE_CHAR){
                 // Not in a word and hit another space.
                 continue;
             } else {
                 // Hits first letter of new word.
-                printf("%d. %c", wc, buff[i]);
+                printf("%d. %c", wc, current);
                 wc++;
                 word_start = true;
                 wlen++;
 
                 // Checks if current index is last and returns wordcount.
                 if (i == last_char_idx){
-                    printf(" (%d)\n", wlen);
+                    printf("(%d)\n", wlen);
                 }
             }
-        
         }
     }
+
+    printf("\nNumber of words returned: %d\n", wc - 1);
+}
+
+void search_and_replace(char *buff, int buffer_len, int str_len, char *old_word, char *new_word){
+    int search_idx = -1, search_end_idx, old_idx = 0;
+    char current;
+
+/*iterates through the buffer looking for the the word that is getting replaced (old_word). When a 
+potential word is found, the index for the first character is stored (search_idx). It then checks 
+to make sure the entire word is there, storing the last index if it is.*/
+    for(int buff_idx = 0; buff_idx < str_len; buff_idx++){
+        current = *(buff + buff_idx);
+
+        if(search_idx == -1){
+            if(current == *(old_word + old_idx)){
+                search_idx = buff_idx;
+                old_idx++;
+            }
+            else {
+                continue;
+            }
+        }
+        else {
+            if(current == old_word[old_idx]){
+                old_idx++;
+                if(*(old_word + old_idx) == '\0'){
+                    search_end_idx = buff_idx;
+                    break;
+                }
+            }
+            else {
+                old_idx = 0;
+                search_idx = -1;
+            }
+        }
+    }
+
+    if (search_idx == -1){
+        exit(1);
+    }
+    
+/*a temporary buffer is created (temp). The start of buff to the search_idx is set to temp. Then, the new word is placed
+in the spot that the old word would be. Then the rest of the original buff string is set to temp. The periods are added to the
+end of temp. Finally, temp is memset to buff, overriding the original with temp.*/
+    char *temp = (char *)malloc(buffer_len * sizeof(char *));
+
+    for(int idx = 0; idx < search_idx; idx++){
+        *(temp + idx) = *(buff + idx);
+    }
+
+    int replacement_idx = 0, temp_idx = search_idx;
+    char replacement = *(new_word);
+    while (replacement != '\0'){
+        *(temp + temp_idx) = replacement;
+        temp_idx++;
+        replacement_idx++;
+        replacement = *(new_word + replacement_idx);
+    }
+
+    for(int idx = search_end_idx + 1; idx < str_len && idx < buffer_len; idx++){
+        *(temp + temp_idx) = *(buff + idx);
+        temp_idx++;
+    }
+
+    memset(temp + temp_idx, '.', (buffer_len + temp_idx) * sizeof(char));
+    memcpy(buff, temp, buffer_len);
+    
+    free(temp);
 }
 
 //ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
@@ -164,8 +255,6 @@ int main(int argc, char *argv[]){
     char opt;               //used to capture user option from cmd line
     int  rc;                //used for return codes
     int  user_str_len;      //length of user supplied string
-    
-    char *reversed;
 
     //TODO:  #1. WHY IS THIS SAFE, aka what if arv[1] does not exist?
     //      PLACE A COMMENT BLOCK HERE EXPLAINING
@@ -211,7 +300,7 @@ int main(int argc, char *argv[]){
     //          return code of 99
     // CODE GOES HERE FOR #3
 
-    buff = (char *)malloc(BUFFER_SZ * sizeof(char));
+    buff = (char *)malloc(BUFFER_SZ * sizeof(char *));
     if (buff == NULL){
         printf("Buffer failed to initialize.");
         free(buff);
@@ -233,25 +322,21 @@ int main(int argc, char *argv[]){
                 exit(2);
             }
             printf("Word Count: %d\n", rc);
+            print_buff(buff,BUFFER_SZ);
             break;
         case 'r':
-            reversed = (char *)malloc(user_str_len * sizeof(char));
-            if (reversed == NULL){
-                printf("Reversed failed to initialize.");
-                free(reversed);
-                exit(99);
-            };
-
-            reverse_words(buff, reversed, user_str_len);
-            printf("Reversed String: %s\n", reversed);
-            free(reversed);
+            reverse_words(buff, user_str_len);
+            print_buff(buff,BUFFER_SZ);
             break;
         case 'w':
             print_words(buff, user_str_len);
+            print_buff(buff,BUFFER_SZ);
             break;
         case 'x':
             if (argc == 5){
-                printf("Not implemented!\n");
+                search_and_replace(buff, BUFFER_SZ, user_str_len, argv[3], argv[4]);
+                print_buff(buff,BUFFER_SZ);
+                //printf("Not Implemented!\n");
             } else {
                 printf("Invalid number of arguments.\n");
                 usage(argv[0]);
@@ -267,7 +352,6 @@ int main(int argc, char *argv[]){
 
     //TODO:  #6 Dont forget to free your buffer before exiting
     free(buff);
-    print_buff(buff,BUFFER_SZ);
     exit(0);
 }
 
