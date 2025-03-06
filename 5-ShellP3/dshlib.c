c7804ecd492a45376705c858e179593281f0e155
rexbeck <reaganrbeck@gmail.com>
2025-03-06 11:20:32 -0500

    started implementing build_cmd_list

--- 5-ShellP3/dshlib.c
+++ 5-ShellP3/dshlib.c
@@ -52,8 +52,118 @@
  *  Standard Library Functions You Might Want To Consider Using (assignment 2+)
  *      fork(), execvp(), exit(), chdir()
  */
+
+int build_cmd_list(char *cmd_line, command_list_t *clist)
+{
+    
+    char *pipe = strstr(cmd_line, "|");
+    if (pipe == NULL)
+    {
+
+    }
+    else
+    {
+        char *pipe_token = strtok(cmd_line, "|");
+        while (pipe_token != NULL)
+        {
+            
+            pipe_token = strtok(NULL, "|");
+        }
+    }
+}
+
+
 int exec_local_cmd_loop()
 {
-   
-    return OK;
+    char *cmd_buff, first;
+    int rc = 0;
+    cmd_buff_t cmd;
+
+    while(true){
+        cmd_buff = malloc(SH_CMD_MAX);
+        printf("%s", SH_PROMPT);
+        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
+            printf("\n");
+            rc = OK;
+            break;
+        }
+
+        //remove the trailing \n from cmd_buff
+        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
+
+        // CHECK FOR EMPTY COMMAND
+        first = *cmd_buff;
+        if (first == '\0')
+        {
+            printf(CMD_WARN_NO_CMD);
+            rc = WARN_NO_CMDS;
+            break;
+        }
+
+        cmd.argc = 0;
+        memset(cmd.argv, '\0', sizeof(cmd.argv));
+        cmd._cmd_buffer = cmd_buff;
+        
+        char *idx = strstr(cmd_buff, "\"");
+
+        if (idx == NULL) 
+        {
+            char *token = strtok(cmd_buff, " ");
+            while (token != NULL){
+                cmd.argv[cmd.argc] = strdup(token);
+                cmd.argc++;
+                token = strtok(NULL, " ");
+            }
+        }
+        else {
+            char *quote_token = strtok(cmd_buff, "\"");
+            quote_token = strtok(NULL, "\"");
+            
+            char *space_token = strtok(cmd_buff, " ");
+            cmd.argv[0] = strdup(space_token);
+            cmd.argv[1] = strdup(quote_token);
+            cmd.argc = 2;
+        }
+
+        if (strcmp(cmd.argv[0], EXIT_CMD) == 0)
+        {
+            rc = OK;
+            break;
+        }
+        else if (strcmp(cmd.argv[0], "cd") == 0)
+        {
+            if (cmd.argc == 1) continue;
+            else if (cmd.argc > 2){
+                printf("Command \"cd\" only one (1) argument.\n");
+                continue;
+            }
+            
+            if (chdir(cmd.argv[1]) != 0){
+                printf("chdir to %s failed\n", cmd.argv[1]);
+                continue;
+            }
+        }
+        else {
+            pid_t pid = fork();
+            if (pid == 0) {
+                // Child process
+                rc = execvp(cmd.argv[0], cmd.argv);
+                if (rc == -1) {
+                    printf("Execvp %s failed", cmd.argv[0]);
+                    rc = ERR_EXEC_CMD;
+                    break;
+                }
+            } else if (pid > 0) {
+                // Parent process
+                wait(NULL);
+            } else {
+                printf("Fork error");
+                rc = ERR_EXEC_CMD;
+                break;
+            }
+        }
+    }
+
+    free(cmd_buff);
+    return rc;
 }
